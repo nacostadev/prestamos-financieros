886425b635add647bf62363baad79462c3ae755b
Naylin Acosta <nacostadev@gmail.com>
2026-09-11 12:01:08 -0500

    fix: corregir a maven

---
+++ src/main/java/com/mycompany/sistemaprestamosfinancieros/SistemaPrestamosFinancieros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaprestamosfinancieros;
+
+/**
+ *
+ * @author user
+ */
+public class SistemaPrestamosFinancieros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
