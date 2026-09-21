c9c4a1130864c1387976f09eae0a5d5b5e097e18
Aaron <anvasquezza@ucvvirtual.edu.pe>
2026-09-20 23:52:38 -0500

    Models Prestamos

--- src/main/java/com/prestamos/model/prestamo/EstadosPrestamo.java
+++ src/main/java/com/prestamos/model/prestamo/EstadosPrestamo.java
@@ -1,4 +1,47 @@
 package com.prestamos.model.prestamo;
 
+/**
+ *
+ * @author LENOVO
+ */
 public class EstadosPrestamo {
+
+    private String estadosPrestamoCodigo;
+    private String estadosPrestamoNombre;
+    private String estadosPrestamoEstado;
+
+    public EstadosPrestamo() {
+    }
+
+    public EstadosPrestamo(String estadosPrestamoCodigo,
+                           String estadosPrestamoNombre,
+                           String estadosPrestamoEstado) {
+        this.estadosPrestamoCodigo = estadosPrestamoCodigo;
+        this.estadosPrestamoNombre = estadosPrestamoNombre;
+        this.estadosPrestamoEstado = estadosPrestamoEstado;
+    }
+
+    public String getEstadosPrestamoCodigo() {
+        return estadosPrestamoCodigo;
+    }
+
+    public void setEstadosPrestamoCodigo(String estadosPrestamoCodigo) {
+        this.estadosPrestamoCodigo = estadosPrestamoCodigo;
+    }
+
+    public String getEstadosPrestamoNombre() {
+        return estadosPrestamoNombre;
+    }
+
+    public void setEstadosPrestamoNombre(String estadosPrestamoNombre) {
+        this.estadosPrestamoNombre = estadosPrestamoNombre;
+    }
+
+    public String getEstadosPrestamoEstado() {
+        return estadosPrestamoEstado;
+    }
+
+    public void setEstadosPrestamoEstado(String estadosPrestamoEstado) {
+        this.estadosPrestamoEstado = estadosPrestamoEstado;
+    }
 }
